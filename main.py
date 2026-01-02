cc4335cdf313bc16a82fdb46d3509d2843de8b14
Bunny <andrewberlinghoff@gmail.com>
2026-01-02 13:43:52 -0500

    added sorting features and final report to book bot. next step is making sure it can analyze any book

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from stats import num_of_words, num_of_characters
+from stats import num_of_words, num_of_characters, report
 
 def get_book_text(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
 
+text = get_book_text("books/frankenstein.txt")
+
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-    num_of_words(get_book_text("books/frankenstein.txt"))
-    print(num_of_characters(get_book_text("books/frankenstein.txt")))
+
+
+    print("============ BOOKBOT ============\n"
+    "Analyzing book found at books/frankenstein.txt...\n"
+    "----------- Word Count ----------")
+    print(f"Found {num_of_words(text)} total words")
+    print("--------- Character Count -------")
+
+    chars = num_of_characters(text)
+    sorted_chars = report(chars)
+
+    for item in sorted_chars:
+        ch = item["char"]
+        count = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {count}")
+
+    print("============= END ===============")
 main()
 
 
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def num_of_words(text):
-    print(f"Found {len(text.split())} words")
-
+    words = text.split()
+    return len(words)
 
 def num_of_characters(text):
     characters = {}
@@ -11,3 +11,14 @@ def num_of_characters(text):
         else:
             characters[char] += 1
     return characters
+
+def dict_num(items):
+    return items["num"]
+
+
+def report(dictionary):
+    list_of_chars = []
+    for key, value in dictionary.items():
+        list_of_chars.append({"char": key,"num": value })
+    list_of_chars.sort(reverse=True, key=dict_num)
+    return list_of_chars
